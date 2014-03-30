cacede18b67ca9c63305c5ae80a7694d3084244c
Victor J. Reventos <victor.reventos1@gmail.com>
2014-03-30 12:04:12 -0400

    Could it be? IV

--- app/scripts/services/torrents.js
+++ app/scripts/services/torrents.js
@@ -1,8 +1,17 @@
 'use strict';
 
 angular.module('castThatPopcornApp')
-  .service('Torrents', function Torrents($http) {
-    this.getTorrents = function() {
-      return $http.get('/api/getTorrents');
-    };
-  });
+.service('Torrents', function Torrents($http) {
+  this.getTorrents = function() {
+    return $http.get('/api/getTorrents');
+  };
+});
+
+angular.module('castThatPopcornApp')
+.service('RottenTomatoes', function Rottentomatoes($http) {
+  this.getRatingFor = function(movieName) {
+    return $http.get('/api/getTomatoes/' + movieName, {
+      cache: true
+    });
+  };
+});
