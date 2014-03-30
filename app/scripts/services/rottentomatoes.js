d658fea03e38b8053a5b659b61c75b9a061c59c5
Victor J. Reventos <victor.reventos1@gmail.com>
2014-03-30 11:32:10 -0400

    dskfsalkfsd

--- app/scripts/services/rottentomatoes.js
+++ app/scripts/services/rottentomatoes.js
@@ -2,8 +2,6 @@
 
 angular.module('castThatPopcornApp')
 .service('RottenTomatoes', function Rottentomatoes($http) {
-
-
   this.getRatingFor = function(movieName) {
     return $http.get('/api/getTomatoes/' + movieName, {
       cache: true
